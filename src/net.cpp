24294c1cfb067ccf048147efeb9746fee3200958
poseidondev <34919737+poseidondev@users.noreply.github.com>
2018-01-02 13:41:23 +0000

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1173,7 +1173,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"walletbuilders.com", "node.walletbuilders.com"},
+    {"192.243.103.201", "192.243.103.201"},
     {"35.202.169.170", "35.202.169.170"},
     {NULL, NULL}
 };
